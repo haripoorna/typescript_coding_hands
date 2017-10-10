42b1ea6fa80ded92884c394df21ec0876a93c3d5
LAPTOP-6B2P6FL8\Hari poorna <haripoornakumar@gmail.com>
2017-10-10 13:33:10 +0530

    explicit object "task"

--- basics.ts
+++ basics.ts
@@ -43,3 +43,20 @@ console.log(entriesArray)
 for (let e of iterator) {
   console.log(e);
 }
+//Try to be as explicit as possible and add Types to everything you can!
+let bankAccount = {
+    money: 2000,
+    deposit(value) {
+        this.money += value;
+    }
+};
+ 
+let myself = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+ 
+myself.bankAccount.deposit(3000);
+ 
+console.log(myself);
